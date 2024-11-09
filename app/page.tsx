3a86af1e4754269f6d8eac8b9778a71d4be31784
Arysmarcio <arystharcomarcio@gmail.com>
2024-11-09 00:15:46 -0300

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import styles from 'app/ui/home.module.css'
 import { lusitana } from './ui/fonts';
 import Image from 'next/image';
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className={styles.shape}>
+      <div className="h-0 w-0 border-b-[30px] border-b-black border-l-[20px] border-l-transparent border-r-[20px] border-r-transparent">
         <AcmeLogo></AcmeLogo>
    
       </div>
